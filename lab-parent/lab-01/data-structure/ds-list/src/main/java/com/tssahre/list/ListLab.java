c46d86f523a55be05c32c980127a30564854a7de
916927902@qq.com <916927902@qq.com>
2020-06-12 00:38:54 +0800

    2、ds -list 开始

---
+++ lab-parent/lab-01/data-structure/ds-list/src/main/java/com/tssahre/list/ListLab.java
@@ -0,0 +1,23 @@
+package com.tssahre.list;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author chenzhenfei
+ * @title: ListLab
+ * @projectName framework-lab
+ * @description: TODO
+ * @date 2020/6/120:22
+ */
+public class ListLab {
+
+    public static void main(String[] args) {
+        Collection c =new ArrayList();
+        List a =new VirtualFlow.ArrayLinkedList<>();
+    }
+}
