ae57a06ae2c51fbb0ff24b50565f1f5728974de9
chenzhenfei <11100247>
2020-06-19 14:35:50 +0800

    1、架构实验室- tree 结束

--- lab-parent/lab-01/data-structure/ds-tree/src/main/java/com/tsshare/tree/BitTree.java
+++ lab-parent/lab-01/data-structure/ds-tree/src/main/java/com/tsshare/tree/BitTree.java
@@ -1,5 +1,10 @@
 package com.tsshare.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 /**
  * @author chenzhenfei
  * @title: BitTree
@@ -8,37 +13,117 @@ package com.tsshare.tree;
  * @date 2020/6/1722:15
  */
 public class BitTree<T> {
+    private int size;
+
+    /***
+     * @Description:根节点
+     * @Author: zfchen
+     * @Date: 2020/6/19 14:09
+     **/
     private TreeNode<T> root;
 
-    public boolean add(TreeNode<T> t){
-        TreeNode<T> objectTreeNode =t;
-        if(root ==null){
+    public boolean add(TreeNode<T> t) {
+        TreeNode<T> objectTreeNode = t;
+        if (root == null) {
             root = objectTreeNode;
-        }else{
-           // 找到合适的点
-            TreeNode findNode =root;
-            while(true){
-                if(findNode.compareTo(objectTreeNode)<0){
-                    TreeNode leftNode = findNode.getLeftNode();
-                    if(leftNode == null){
-                        findNode.setLeftNode(leftNode);
+        } else {
+            // 找到合适的点
+            TreeNode<T> findNode = root;
+            while (true) {
+                if (findNode.compareTo(objectTreeNode) > 0) {
+                    TreeNode<T> leftNode = findNode.getLeftNode();
+                    if (leftNode == null) {
+                        findNode.setLeftNode(objectTreeNode);
                         break;
-                    }else{
-                        findNode =  leftNode;
+                    } else {
+                        findNode = leftNode;
                     }
-                }else{
-                    TreeNode rightNode = findNode.getRightNode();
-                    if(rightNode == null){
-                        findNode.setRightNode(rightNode);
+                } else {
+                    TreeNode<T> rightNode = findNode.getRightNode();
+                    if (rightNode == null) {
+                        findNode.setRightNode(objectTreeNode);
                         break;
-                    }else{
-                        findNode =  rightNode;
+                    } else {
+                        findNode = rightNode;
                     }
                 }
             }
         }
+        size++;
         return true;
     }
 
 
+    /***
+     * @Description:遍历 ,可选择便利方式
+     * @Author: zfchen
+     * @Date: 2020/6/19 9:39
+     **/
+    public void forEach(Consumer<? super T> action) {
+        // 用于遍历临时存储
+        List<T> list ;
+        Objects.requireNonNull(action);
+//        list=  preOrder(root);
+//        list =inOrder(root);
+        list =backOrder(root);
+        for (int i = 0; i <list.size() ; i++) {
+            action.accept(list.get(i));
+        }
+
+    }
+
+    /**
+     * 前序遍历  中左右
+     * @param root
+     * @return
+     */
+
+    private List<T> preOrder(TreeNode<T> root) {
+        List<T> list = new ArrayList<>();
+        if(root!=null){
+            list.add(root.getData());
+            preOrder(root.getLeftNode());
+            preOrder(root.getRightNode());
+        }
+        return list;
+    }
+
+    /**
+     * 中序遍历 左中右
+     * @param root
+     * @return
+     */
+    private List<T> inOrder(TreeNode<T> root) {
+        List<T> list = new ArrayList<>();
+        if(root!=null){
+            inOrder(root.getLeftNode());
+            list.add(root.getData());
+            inOrder(root.getRightNode());
+        }
+        return list;
+    }
+
+    /**
+     * 后序遍历 左右中
+     * @param root
+     * @return
+     */
+    private List<T> backOrder(TreeNode<T> root) {
+        List<T> list = new ArrayList<>();
+        if(root!=null){
+            backOrder(root.getLeftNode());
+            backOrder(root.getRightNode());
+            list.add(root.getData());
+        }
+        return list;
+    }
+
+
+
+    public int size(){
+        return this.size;
+    }
+    public Boolean isEmpty(){
+        return this.size == 0;
+    }
 }
--- lab-parent/lab-01/data-structure/ds-tree/src/main/java/com/tsshare/tree/TreeLab.java
+++ lab-parent/lab-01/data-structure/ds-tree/src/main/java/com/tsshare/tree/TreeLab.java
@@ -13,12 +13,20 @@ import java.util.Map;
  */
 public class TreeLab {
     public static void main(String[] args) {
-       BitTree<Integer> bit =new BitTree<Integer>();
-        for (int i = 0; i <10 ; i++) {
-            TreeNode<Integer> t =new TreeNode<>();
+        BitTree<Integer> bit = new BitTree<Integer>();
+        for (int i = 5; i < 10; i++) {
+            TreeNode<Integer> t = new TreeNode<>();
             t.setIndex(i);
             t.setData(i);
             bit.add(t);
         }
+        for (int i = 1; i < 5; i++) {
+            TreeNode<Integer> t = new TreeNode<>();
+            t.setIndex(i);
+            t.setData(i);
+            bit.add(t);
+        }
+        bit.forEach(System.out::println);
+
     }
 }
