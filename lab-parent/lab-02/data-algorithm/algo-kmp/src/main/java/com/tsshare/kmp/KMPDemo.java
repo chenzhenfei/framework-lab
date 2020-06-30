b889cf5e6c328614caf2c78ad06946fd8a60d1e8
916927902@qq.com <916927902@qq.com>
2020-07-01 00:29:56 +0800

    1、algo-kmp 字符串匹配

---
+++ lab-parent/lab-02/data-algorithm/algo-kmp/src/main/java/com/tsshare/kmp/KMPDemo.java
@@ -0,0 +1,11 @@
+package com.tsshare.kmp;
+
+/**
+ * @author chenzhenfei
+ * @title: KMPDemo
+ * @projectName framework-lab
+ * @description:
+ * @date 2020/7/10:11
+ */
+public class KMPDemo {
+}
---
+++ lab-parent/lab-02/data-algorithm/algo-kmp/src/main/java/com/tsshare/kmp/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @title: package-info
+ * @projectName framework-lab
+ * @description: TODO
+ * @author chenzhenfei
+ * @date 2020/7/10:12
+ */
+package com.tsshare.kmp;
+
+//KMP：Knuth-Morris-Pratt算法（简称KMP） 核心原理是利用一个“部分匹配表”，跳过已经匹配过的元素。
