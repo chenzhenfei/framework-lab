494b4b3149d62f896032cfbc443d736bf4f5ad30
916927902@qq.com <916927902@qq.com>
2020-09-13 23:00:14 +0800

    1、生产者/消费者 demo

---
+++ lab-parent/lab-06/src/main/java/com/tsshare/lab06/ProductorConsumerDemo.java
@@ -0,0 +1,93 @@
+package com.tsshare.lab06;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author chenzhenfei
+ * @title: ProductorConsumerDemo
+ * @projectName framework-lab
+ * @description: 生产者/消费者   生产者/消费着案例
+ *                  1、notify 早期通知
+ *                  2、假死
+ * @date 2020/9/1322:33
+ */
+public class ProductorConsumerDemo {
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(15);
+        for (int i = 0; i < 5; i++) {
+            executorService.submit(new Productor(integers, 8));
+        }
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Consumer(integers));
+        }
+    }
+
+
+    static class Productor implements Runnable {
+        private List<Integer> list;
+        private int length;
+
+        public Productor(List<Integer> list, int length) {
+            this.list = list;
+            this.length = length;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (list) {
+                    try {
+                        while (list.size() == length) {
+                            System.out.println(Thread.currentThread().getName() + "：生产者已经达到峰值");
+                            list.wait();
+                            System.out.println(Thread.currentThread().getName() + "：退出wait");
+                        }
+                        Random random = new Random();
+                        list.add(random.nextInt());
+                        // 唤醒其他线程
+                        list.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+        private List<Integer> list;
+
+        public Consumer(List<Integer> list) {
+            this.list = list;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (list) {
+                    try {
+                        while (list.size() == 0) {
+                            System.out.println(Thread.currentThread().getName() + "：消费者已经为0");
+                            list.wait();
+                            System.out.println(Thread.currentThread().getName() + "：退出wait");
+                        }
+                        Integer remove = list.remove(0);
+                        list.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
--- lab-parent/lab-06/src/main/java/com/tsshare/lab06/ThreadWaitDemo.java
+++ lab-parent/lab-06/src/main/java/com/tsshare/lab06/ThreadWaitDemo.java
@@ -4,19 +4,18 @@ package com.tsshare.lab06;
  * @author chenzhenfei
  * @title: WaitDemo
  * @projectName framework-lab
- * @description: wait /notify
+ * @description: wait /notify    wait 会让当前获得锁得线程进入等待队列（waitting）,
+ *                             ：notify 启动之后会唤醒 waitting 中得一个线程，进入一个同步队列中，等待获取锁
+ *                             ：notifyAll 启动之后会将所有watting所有得线程唤醒，进入到同步队列中等待，排队获取锁
+ *
  * @date 2020/9/1321:14
  */
 public class ThreadWaitDemo {
     private String objLock = "wait";
 
-
-
-
     public static void main(String[] args) {
         test2();
     }
-
 
     private static void test2() {
         ThreadWaitDemo threadWaitDemo = new ThreadWaitDemo();
@@ -54,6 +53,7 @@ public class ThreadWaitDemo {
             try {
                 System.out.println(Thread.currentThread().getName() + "：唤醒其他线程");
                 Thread.sleep(2000);
+                //调用 notify 后，当前线程不会马上释放该对象锁，要等到程序退出同步块后，当前线程才会释放锁。
                 objLock.notify();
                 objLock.wait();
                 System.out.println(Thread.currentThread().getName() + "：重新获得锁");
